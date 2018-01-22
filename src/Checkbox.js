38b47aa8e4fd3b6ffe07c18e5ec36912532fc0cf
InshaNazeer <insha@xelpmoc.in>
2018-01-22 17:35:12 +0530

    DoneChanges

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,16 +1,13 @@
 import React from "React";
 import PropTypes from "prop-types";
 export default class Checkbox extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   handleChange(val) {
   this.setState({
     value: val.target.value
   });
 }
   render() {
-    return <input disabled={this.props.disabled}  checked={this.props.checked} type="checkbox" onChange={this.props.onChange} />
+    return <input  type="checkbox" {...this.props} />
   }
 }
 Checkbox.propTypes = {
--- src/Input.js
+++ src/Input.js
@@ -7,22 +7,14 @@ export default class Input extends React.Component {
     this.state={
       value:'default',
     }
-   // this.handleOnChange=this.handleOnChange.bind(this);
   }
   handleOnChange =(e) =>{
     this.setState({value: e.target.value});
   }
-  // handleOnChange(e) {
-  //   console.log( e.target.value);
-  //   this.setState({value: e.target.value});
-  //   console.log(this.state.value);
-  // }
   render() {
     return(
       <input 
       value={this.state.value}  onChange={this.handleOnChange}/>
-      // <input 
-      // value={this.state.value}  onChange={(val) => this.handleOnChange(val)}/>
   );
   }
 }
