2177fd613c668c0a6c7473997bc44b36fb224500
abi121 <abhishek@xelpmoc.in>
2018-02-02 09:49:15 +0530

    fix the fat arrow issue

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -6,13 +6,12 @@ import PropTypes from "prop-types";
 export default class Checkbox extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = () => this.handleChange();
   }
-  handleChange(val) {
+  handleChange = val => {
     this.setState({
       value: val.target.value
     });
-  }
+  };
 
   render() {
     return <input type="checkbox" {...this.props} />;
